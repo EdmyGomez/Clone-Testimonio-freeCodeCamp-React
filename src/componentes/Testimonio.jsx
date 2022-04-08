ea65164fe319acfb35de87381eabb324efb1328a
Edmy Gomez <edgomez039@misena.edu.co>
2022-04-08 12:35:30 -0500

    Ajustes en el Estilo

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import '../styles/Testimonio.css'
+import React from "react";
+import "../styles/Testimonio.css";
 
-function Testimonio(props){
-  return(
+function Testimonio(props) {
+  return (
     <div className="contenedor-testimonio">
       {/* Crear imagenes sencillas con React */}
       {/* <img className="imagen-testimonio" src={require("../imagenes/aiony-haust-3TLl_97HNJo-unsplash.jpg")} alt="Foto de Aiony" srcset="" /> */}
       {/* Uso de Comillas invertidas para poder usar props en las imagenes */}
 
-      <img className="img-fluid rounded img-responsive imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt={`Foto de ${props.nombre}`} srcset="" />
+      <img
+        className="img-fluid rounded img-responsive imagen-testimonio"
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
+        alt={`Foto de ${props.nombre}`}
+        srcset=""
+      />
 
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
@@ -17,11 +22,8 @@ function Testimonio(props){
         <p className="cargo-testimonio">
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
-        <p className="texto-testimonio">
-          "{props.testimonio}"
-        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
-    
     </div>
   );
 }
